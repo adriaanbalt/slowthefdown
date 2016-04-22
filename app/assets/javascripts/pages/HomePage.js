bea682b82a8a9260a0cba7db410750386f70c2f4
adriaanbalt <adriaan@balt.us>
2016-04-22 12:23:18 -0400

    adding pixi F

--- app/assets/javascripts/pages/HomePage.js
+++ app/assets/javascripts/pages/HomePage.js
@@ -3,17 +3,34 @@
 import React, { PropTypes } from 'react';
 import cx from 'classnames';
 import { connect } from 'react-redux';
+import ReactPIXI from 'react-pixi';
+import PIXI from 'pixi.js';
 
 import UI from '../lib/UI';
+import F from '../components/F';
 import API from '../redux/API';
 
 class HomePage extends UI {
 
-  render () {
-    console.log ( "Rnder HomePage" )
-      return (
+  constructor( props ) {
+    super( props );
+    // let Stage = React.createFactory(ReactPIXI.Stage);
+    // let Sprite = React.createFactory(ReactPIXI.Sprite);
+    // let DisplayObjectContainer = React.createFactory(ReactPIXI.DisplayObjectContainer);
+    // let TilingSprite = React.createFactory(ReactPIXI.TilingSprite);
+    // let VectorText = React.createFactory(ReactPIXI.Text);
+    // let BitmapText = React.createFactory(ReactPIXI.BitmapText);
 
-        <div>Hello World!</div>
+  }
+
+  render () {
+    let Stage = React.createFactory(ReactPIXI.Stage);
+    let DisplayObjectContainer = React.createFactory(ReactPIXI.DisplayObjectContainer);
+    console.log ( "Render HomePage" )
+      return Stage(
+        {width: window.innerWidth, height: window.innerHeight, backgroundcolor: 0xa08080, interactive:true},
+        // children components are the buttons and the dynamic sprites
+        React.createElement(F, {key:'gui'}),
       );
   }
 
