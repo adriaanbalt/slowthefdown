55f36371731b872b4eae26a838f6e227b8d698a1
adriaanbalt <adriaan@balt.us>
2016-05-21 16:56:41 -0400

    fixing webpack production task to run on correct port

--- gulpfile.js/tasks/webpack-production.js
+++ gulpfile.js/tasks/webpack-production.js
@@ -7,6 +7,6 @@ var gulp    = require('gulp'),
 gulp.task('webpack:production', function(callback) {
   webpack(config, function(err, stats) {
     logger(err, stats);
-    // server(callback);
+    server(callback, process.env.PORT);
   });
 });
