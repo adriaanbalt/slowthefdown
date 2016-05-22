d4c4d5f64ac49a8e6f9ca3fa679bd3ded5b1632e
adriaanbalt <adriaan@balt.us>
2016-05-22 12:10:26 -0400

    adding login

--- app/assets/javascripts/components/Footer.js
+++ app/assets/javascripts/components/Footer.js
@@ -4,12 +4,16 @@ import React from 'react';
 import { Link } from 'react-router';
 
 import Icon from './Icon';
+import Login from './Login';
 
 function Footer( props ) {
+
+	let drawerOpen = props.drawerOpen ? ' drawerOpen' : '';
+	let drawerPeak = props.drawerPeak ? ' drawerPeak' : '';
 	return (
-		<footer className={`Footer ${ props.drawerOpen }`}>
+		<footer className={`Footer${ drawerOpen }${ drawerPeak }`}>
 			<div className="info">
-				<h6 className="highscore-small">{ props.highscore.highscore }</h6>
+				<h6 className="highscore-small">{ props.score }</h6>
 				<a className="drawerBtn" onTouchStart={ props.toggleDrawer } onClick={ props.toggleDrawer }>				
 					<Icon name="icon-menu"/>	
 				</a>
@@ -17,10 +21,11 @@ function Footer( props ) {
 			<div className="drawer">
 				<div className="inner">
 					<div className="highscore-container">
-						<h3>HighScore</h3>
+						<h3>High Score</h3>
 						<h3 className="highscore-big">{ props.highscore.highscore }</h3>
 					</div>
-					<p>BALT.us  Copyright 2016</p>
+					<Login />
+					<small><a href="http://balt.us">BALT.us</a>  Copyright 2016</small>
 				</div>
 			</div>
 		</footer>
