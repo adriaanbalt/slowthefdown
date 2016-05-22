364367309fefd8fa059924ae20fa145fc364cfa3
adriaanbalt <adriaan@balt.us>
2016-05-22 12:11:49 -0400

    highscore / api route to lowercase

--- server/apiRoutes/highscore/index.js
+++ server/apiRoutes/highscore/index.js
@@ -12,13 +12,13 @@ Promise.promisifyAll(HighScore.prototype);
 module.exports = (router) => {
 
   // Get initial app data
-  router.get('/HighScore', function(req, res, next) {
+  router.get('/highscore', function(req, res, next) {
     HighScore.findAsync({ creator: req.session.passport.user }, null, {sort:'-createdDate'})
       .then(allHighScores => res.json(allHighScores))
       .catch(err => !console.log(err) && next(err));
   });
 
-  router.post('/HighScore', function(req, res, next) {
+  router.post('/highscore', function(req, res, next) {
     let tempId = shortid.generate()
     const newHighScore = new HighScore(_.merge({ _id: Number(new Date()).toString() + '_' + tempId }, req.body));
 
@@ -29,19 +29,19 @@ module.exports = (router) => {
       .catch(err => !console.log(err) && next(err));
   });
 
-  router.put('/HighScore/:id', function(req, res, next) {
+  router.put('/highscore/:id', function(req, res, next) {
     HighScore.findByIdAndUpdateAsync(req.params.id, req.body, {new:true}) // new option here says return the updated object to the following promise
       .then(updatedHighScore => res.status(200).json(updatedHighScore))
       .catch(err => !console.log(err) && next(err));
   });
 
-  router.get('/HighScore/:id', function(req, res, next) {
+  router.get('/highscore/:id', function(req, res, next) {
     HighScore.findById(req.params.id)
       .then(obj => res.status(200).json(obj))
       .catch(err => !console.log(err) && next(err));
   });
 
-  router.delete('/HighScore/:id', function(req, res, next) {
+  router.delete('/highscore/:id', function(req, res, next) {
     // only delete if the user id is the same as passport...
     HighScore.findByIdAsync(req.params.id)
       .then( checkHighScore => {
