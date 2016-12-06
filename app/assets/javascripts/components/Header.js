233b2cf2051704f632ceea52035ba0d70f8b4f2c
adriaanbalt <adriaan@balt.us>
2016-12-05 23:24:26 -0500

    header styles adding the progress and highscore back

--- app/assets/javascripts/components/Header.js
+++ app/assets/javascripts/components/Header.js
@@ -5,10 +5,11 @@ import Navigation from './navigation/Navigation';
 
 function Header( props ) {
   return (
-      <header className='Header'>
-      	<Navigation />
-		<h4 className="highscore small">{ props.score }</h4>
-      </header>
+	  <header className='Header'>
+		<Navigation />
+		<h4 className="highscore big"><span className="copy">Highscore:</span>{ props.highscore }</h4>
+		<h4 className="highscore small"><span className="copy">Progress:</span>{ props.score }</h4>
+	  </header>
   );
 };
 
