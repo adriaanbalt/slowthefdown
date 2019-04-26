8c89555aa7a392e872914f5e11ea1a4b0db456e6
adriaanbalt <adriaan@balt.us>
2019-04-25 21:01:22 -0400

    creating LEVELS class

---
+++ src/screens/Home/levels/index.js
@@ -0,0 +1,10 @@
+export default class Level {
+
+    constructor() {
+        // has an object that will be added to the GLView
+        // has a duration of how long it takes to pass
+        // has a type of visualization
+        // has a Game Center challenge
+        
+    }
+}
