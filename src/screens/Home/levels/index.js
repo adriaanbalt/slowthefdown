457e4f8a2eac395a3aab04b1ffba59cc61dbabc7
adriaanbalt <adriaan@balt.us>
2019-04-25 21:27:49 -0400

    creating the abstract level object and specific vortex level

--- src/screens/Home/levels/index.js
+++ src/screens/Home/levels/index.js
@@ -1,8 +1,10 @@
 export default class Level {
 
-    constructor() {
+    
+    constructor({ duration = 1000, visualization = {} }) {
         // has an object that will be added to the GLView
         // has a duration of how long it takes to pass
+        this.DURATION = duration
         // has a type of visualization
         // has a Game Center challenge
         
