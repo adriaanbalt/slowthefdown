787d685f73e70fdb923e12c7f40fa787191e74e7
Adriaan Balt <adriaan@balt.us>
2019-12-12 12:34:28 -0500

    app.js uses custom Loader directive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import Expo, { Updates } from "expo";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { Provider } from "react-redux";
-import * as Font from "expo-font";
-import { activateKeepAwake, deactivateKeepAwake } from "expo-keep-awake";
-import { Ionicons } from "@expo/vector-icons";
 import store from "./redux/store";
 import AppNavigator from "./navigation/AppNavigator";
 import BannerAd from "./shared/BannerAd";
@@ -24,46 +20,25 @@ export default class App extends React.Component {
 		isLoadingComplete: false,
 	};
 
-	_activate = () => {
-		activateKeepAwake();
-	};
-
-	_deactivate = () => {
-		deactivateKeepAwake();
-	};
-
 	render() {
+		if (!this.state.isLoadingComplete) {
+			return (
+				<Loader
+					onError={this._handleLoadingError}
+					onFinish={this._handleFinishLoading}
+				/>
+			);
+		}
 		return (
 			<Provider store={store}>
-				<React.Fragment>
-					{!this.state.isLoadingComplete && (
-						<Loader
-							onFinish={this._handleFinishLoading}
-							onError={this._handleLoadingError}
-						/>
-					)}
-					{this.state.isLoadingComplete && (
-						<View style={styles.container}>
-							<Modal />
-							<AppNavigator />
-							<BannerAd />
-						</View>
-					)}
-				</React.Fragment>
+				<View style={styles.container}>
+					<Modal />
+					<AppNavigator />
+					<BannerAd />
+				</View>
 			</Provider>
 		);
 	}
-
-	_loadResourcesAsync = async () => {
-		return Promise.all([
-			Font.loadAsync({
-				// This is the font that we are using for our tab bar
-				...Ionicons.font,
-				// We include SpaceMono because we use it in HomeScreen.js. Feel free
-				// to remove this if you are not using it in your app
-			}),
-		]);
-	};
 
 	_handleLoadingError = (error) => {
 		// In this case, you might want to report the error to your error
