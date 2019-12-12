4d7cb8d95d71be76bb115cc750d58aa2cf4fbb0b
Adriaan Balt <adriaan@balt.us>
2019-12-12 12:34:35 -0500

    formatting

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,9 +1,6 @@
 export default {
-  apiKey: "AIzaSyCmP3XNUSjzPNJkMeJIox91SrOiMsd1uok",
-  authDomain: "slowitdown-d1ac8.firebaseapp.com",
-  databaseURL: "https://slowitdown-d1ac8.firebaseio.com",
-  storageBucket: "slowitdown-d1ac8.appspot.com"
+	apiKey: "AIzaSyCmP3XNUSjzPNJkMeJIox91SrOiMsd1uok",
+	authDomain: "slowitdown-d1ac8.firebaseapp.com",
+	databaseURL: "https://slowitdown-d1ac8.firebaseio.com",
+	storageBucket: "slowitdown-d1ac8.appspot.com",
 };
-
-
-
