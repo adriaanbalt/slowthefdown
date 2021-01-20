3e25eb1be92c49c4e791a845c5c41cbc04bd3597
Adriaan Balt <adriaan@balt.us>
2021-01-20 16:41:19 -0500

    firebase auth is working again

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,9 +1,10 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import Constants from 'expo-constants';
+import "firebase/database";
+import Constants from "expo-constants";
 
 const config = Constants.manifest.extra.firebase;
 
 export default !firebase.apps.length
-    ? firebase.initializeApp(config)
-    : firebase.app();
+	? firebase.initializeApp(config)
+	: firebase.app();
--- src/redux/dispatchers.js
+++ src/redux/dispatchers.js
@@ -1,9 +1,10 @@
 import Expo from "expo";
-import * as Updates from 'expo-updates'
+import * as Updates from "expo-updates";
 const actions = require("./actions");
 import store from "./store";
 import * as SecureStore from "expo-secure-store";
 // import * as firebase from "firebase";
+import firebase from "../constants/firebase";
 import { AdMobInterstitial } from "expo-ads-admob";
 import AdMob from "../constants/AdMob";
 import FIREBASE_CONSTANTS from "../constants/firebase";
@@ -24,61 +25,61 @@ export default (dispatch) => () => {
 		// 	set("/initialized", true);
 		// }
 
-		// AdMobInterstitial.setAdUnitID(AdMob.interstitial); // Test ID, Replace with your-admob-unit-id
-		// // AdMobInterstitial.setTestDeviceID('EMULATOR');
-		// AdMobInterstitial.addEventListener("interstitialDidOpen", () =>
-		// 	console.log("interstitialDidOpen"),
-		// );
-		// AdMobInterstitial.addEventListener("interstitialDidClose", () => {
-		// 	console.log("interstitialDidClose");
-		// 	set("/ad/interstitial", false);
-		// });
+		AdMobInterstitial.setAdUnitID(AdMob.interstitial); // Test ID, Replace with your-admob-unit-id
+		// AdMobInterstitial.setTestDeviceID('EMULATOR');
+		AdMobInterstitial.addEventListener("interstitialDidOpen", () =>
+			console.log("interstitialDidOpen"),
+		);
+		AdMobInterstitial.addEventListener("interstitialDidClose", () => {
+			console.log("interstitialDidClose");
+			set("/ad/interstitial", false);
+		});
 
 		// load as existing or new user
 		// check if secure store has info?
 		// check if server has info?
 
 		// check auth login state changes
-		// firebase.auth().onAuthStateChanged(async (user) => {
-		// 	// console.log("!!onAuthStateChanged", user != null);
-		// 	if (user != null) {
-		// 		// get the user's local highscore to compare to the server highscore, take whatever is higher
-		// 		// server is what other users will see but secure store lets users play offline and keep their highscore...
-		// 		// if they play offline they will compete against themselves, but when they go online and they play their new highscore will in fact be updated online
-		// 		// TODO: add this updating online/offline to the ABOUT page
-		// 		const userHsFromSecureStore = await getSecureStoreHighScore();
+		firebase.auth().onAuthStateChanged(async (user) => {
+			// console.log("!!onAuthStateChanged", user != null);
+			if (user != null) {
+				// get the user's local highscore to compare to the server highscore, take whatever is higher
+				// server is what other users will see but secure store lets users play offline and keep their highscore...
+				// if they play offline they will compete against themselves, but when they go online and they play their new highscore will in fact be updated online
+				// TODO: add this updating online/offline to the ABOUT page
+				const userHsFromSecureStore = await getSecureStoreHighScore();
 
-		// 		// get the current user"s highscore
-		// 		firebase
-		// 			.database()
-		// 			.ref("users/" + user.uid)
-		// 			.once("value", (snapshot) => {
-		// 				let hs = snapshot.val() && snapshot.val().highscore;
-		// 				if (!hs || hs < userHsFromSecureStore) {
-		// 					// if server highscore is less than secure store hardware highscore, choose the secure store hardware highscore
-		// 					hs = userHsFromSecureStore;
-		// 					// since server is not as high as local secure store, also update the server
-		// 					const user = firebase.auth().currentUser;
-		// 					updateUserOnDb({
-		// 						uid: user.uid,
-		// 						highscore: hs,
-		// 					});
-		// 				}
-		// 				// set the highscore to whatever the server or secure store hardware value is
-		// 				setUserHighscore(hs);
-		// 			});
+				// get the current user"s highscore
+				firebase
+					.database()
+					.ref("users/" + user.uid)
+					.once("value", (snapshot) => {
+						let hs = snapshot.val() && snapshot.val().highscore;
+						if (!hs || hs < userHsFromSecureStore) {
+							// if server highscore is less than secure store hardware highscore, choose the secure store hardware highscore
+							hs = userHsFromSecureStore;
+							// since server is not as high as local secure store, also update the server
+							const user = firebase.auth().currentUser;
+							updateUserOnDb({
+								uid: user.uid,
+								highscore: hs,
+							});
+						}
+						// set the highscore to whatever the server or secure store hardware value is
+						setUserHighscore(hs);
+					});
 
-		// 		// since user exists, store user data in the reducer
+				// since user exists, store user data in the reducer
 
-		// 		setUserDataLocal(user);
-		// 	} else {
-				// const hs = await getSecureStoreHighScore();
-				// setUserHighscore(hs);
-			// }
+				setUserDataLocal(user);
+			} else {
+				const hs = await getSecureStoreHighScore();
+				setUserHighscore(hs);
+			}
 			// Do other things
-		// });
+		});
 
-		// getHighscores();
+		getHighscores();
 	};
 
 	const toggleModal = (action) => {
@@ -133,21 +134,18 @@ export default (dispatch) => () => {
 
 	const getHighscores = () => {
 		// a promise is used here to create a loader
-		// return new Promise((resolve, reject) => {
-		// 	// get all users" highscores
-		// 	var query = firebase.database().ref("users");
-		// 	query
-		// 		.orderByChild("highscore")
-		// 		.limitToLast(10)
-		// 		.on("value", (snapshot) => {
-		// 			let highscores = [];
-		// 			snapshot.forEach((childSnapshot) => {
-		// 				highscores.push(childSnapshot.val());
-		// 			});
-		// 			set("/highscores", highscores);
-		// 			resolve(highscores);
-		// 		});
-		// });
+		return new Promise((resolve, reject) => {
+			// get all users" highscores
+			var query = firebase.database().ref("users");
+			query.orderByChild("highscore").on("value", (snapshot) => {
+				let highscores = [];
+				snapshot.forEach((childSnapshot) => {
+					highscores.push(childSnapshot.val());
+				});
+				set("/highscores", highscores);
+				resolve(highscores);
+			});
+		});
 	};
 
 	const getSecureStoreHighScore = async () => {
@@ -188,10 +186,10 @@ export default (dispatch) => () => {
 
 	// update the DB user data (happens after each login to ensure redundancy)
 	const updateUserOnDb = (obj) => {
-		// firebase
-		// 	.database()
-		// 	.ref("users/" + obj.uid)
-		// 	.update(obj);
+		firebase
+			.database()
+			.ref("users/" + obj.uid)
+			.update(obj);
 	};
 
 	const setFirstTime = () => {
