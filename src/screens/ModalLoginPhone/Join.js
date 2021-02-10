14adacacace0116d98680ccf151583c1dbee4ea0
Adriaan Balt <adriaan@balt.us>
2021-02-10 16:38:12 -0500

    upgrading modal login phone join class

--- src/screens/ModalLoginPhone/Join.js
+++ src/screens/ModalLoginPhone/Join.js
@@ -1,42 +1,24 @@
 import React from "react";
-import { connect } from "react-redux";
-import { StyleSheet, View } from "react-native";
-import Dispatchers from "../../redux/dispatchers";
+import { View } from "react-native";
 import COLORS from "../../constants/Colors";
 import PhoneAuth from "../../utils/PhoneAuth";
 
-const styles = StyleSheet.create({
-	container: {
-		padding: 15,
-	},
-});
-
-class Join extends React.Component {
-	mount = false;
-	state = {
-		error: null,
-	};
-
-	signUpAuth = (user) => {
-		this.props.setUserDataLocal(user);
-		this.props.navigation.goBack();
-	};
-
-	render() {
-		const { style } = this.props;
-
-		return (
-			<View style={[style, styles.container]}>
-				<PhoneAuth
-					onComplete={this.signUpAuth}
-					onboardingButtonStyle={{
-						backgroundColor: COLORS.backgroundColor,
-						fontColor: COLORS.fontColor,
-					}}
-				/>
-			</View>
-		);
-	}
+export default function Join(props) {
+	return (
+		<View
+			style={{
+				padding: 15,
+			}}>
+			<PhoneAuth
+				onComplete={(user) => {
+					props.setUserDataLocal(user);
+					props.navigation.goBack();
+				}}
+				onboardingButtonStyle={{
+					backgroundColor: COLORS.backgroundColor,
+					fontColor: COLORS.fontColor,
+				}}
+			/>
+		</View>
+	);
 }
-
-export default connect(null, Dispatchers)(Join);
